b82afc2207a6788aa80bbc3b8c44f1acf9f171b7
shrihari R <shrihari3122003@gmail.com>
2023-09-15 21:19:33 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/https://github.com/SHRIHARI31/todoList",
   plugins: [react()],
 })
